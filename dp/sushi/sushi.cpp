70dfcf0fc9f7ea3daa98dad51b38f7d1d2f2ae28
nopanderer <hyunjin7857@gmail.com>
2017-10-12 23:31:19 +0900

    update sushi and trail

--- dp/sushi/sushi.cpp
+++ dp/sushi/sushi.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int t,n;
 int i,j,temp;
 int s[2][100001];
-int d[2][100001];
+int d[100001][3];
 int main() {
   scanf("%d",&t);
   while (t--) {
@@ -13,14 +13,14 @@ int main() {
     for (i=0;i<2;++i) for (j=0;j<n;++j) scanf("%d",&s[i][j]);
 
     for (i=0;i<n;i++) {
-      for (j=0;j<2;j++) {
-        temp = max(max(d[0][i-1],d[1][i-1]), s[j][i] + max(d[0][i-2], d[1][i-2]));
-        d[j][i] = max(temp, s[j][i] + s[(j+1)%2][i-1] + d[j][i-2]);
-      }
+      // 1. not eat -> i-1's max value
+      d[i][0] = max(max(d[i-1][0],d[i-1][1]), d[i-1][2]);
+      // 2. eat upper sushi -> max(not eat i-1's sushi, eat i-1's lower sushi)
+      d[i][1] = max(s[0][i] + max(max(d[i-2][0],d[i-2][1]), d[i-2][2]), s[0][i] + d[i-1][2]);
+      // 3. eat lower sushi
+      d[i][2] = max(s[1][i] + max(max(d[i-2][0],d[i-2][1]), d[i-2][2]), s[1][i] + d[i-1][1]);
     }
-    
-    printf("%d\n",max(d[0][n-1], d[1][n-1]));
+    printf("%d\n",max(max(d[n-1][0], d[n-1][1]), d[n-1][2]));
   }
-
   return 0;
 }
