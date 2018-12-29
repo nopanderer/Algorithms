2bcec83fe35983b0e45a4ab642d79c11515f936f
nopanderer <hyunjin7857@gmail.com>
2018-12-29 19:32:26 +0900

    쉬운 계단 수

--- dp/10844.cpp
+++ dp/10844.cpp
@@ -5,6 +5,7 @@
 #include <cstdio>
 
 long long dp[101][10];
+long long mod = 1000000000;
 int main(){
   int n;
   long long s=0;
@@ -12,16 +13,16 @@ int main(){
   for(int i=1;i<=9;i++)
     dp[1][i] = 1;
   for(int i=2;i<=n;i++){
-    for(int j=1;j<=8;j++){
-      dp[i][j] = dp[i-1][j-1] + dp[i-1][j+1];
-      dp[i][j] %= 1000000000;
+    for(int j=0;j<=9;j++){
+      if(j==0) dp[i][j] = dp[i-1][1];
+      else if(j==9) dp[i][j] = dp[i-1][8];
+      else dp[i][j] = dp[i-1][j-1] + dp[i-1][j+1];
+      dp[i][j] %= mod;
     }
-    dp[i][0] = dp[i-1][1];
-    dp[i][9] = dp[i-1][8];
   }
-  for(int i=1;i<=9;i++)
+  for(int i=0;i<=9;i++)
     s+=dp[n][i];
-  printf("%lld",s%1000000000);
+  printf("%lld",s % mod);
 
   return 0;
 }
