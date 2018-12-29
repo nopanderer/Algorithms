f9421eaded28fe3d00818b7cc7ab0686f4062e0c
nopanderer <hyunjin7857@gmail.com>
2018-12-29 16:21:11 +0900

    2Xn 타일링2

---
+++ dp/11727.cpp
@@ -0,0 +1,21 @@
+/**
+ * 2Xn 타일링2
+ * 11727
+ * NOTE: 중간에 10007로 나눠주지 않으면 오버플로우 발생
+ */
+#include <cstdio>
+
+int dp[1001];
+int main(){
+  int n;
+  scanf("%d",&n);
+  dp[1] = 1;
+  dp[2] = 3;
+  for(int i=3;i<=n;i++){
+    dp[i] = dp[i-2]*2 + dp[i-1];
+    dp[i] %= 10007;
+  }
+  printf("%d",dp[n]);
+  
+  return 0;
+}
