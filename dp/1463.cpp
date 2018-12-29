8baab8dc01b89f0814aec9cef4b2b32ee7f40e3d
nopanderer <hyunjin7857@gmail.com>
2018-12-29 13:24:50 +0900

    1로 만들기

--- dp/1463.cpp
+++ dp/1463.cpp
@@ -6,22 +6,26 @@
 #include <algorithm>
 const int MAX = 1000001;
 
-int dp[1000001];
-int f(int n){
-  if(n == 1) return 0; // base case
-  if(dp[n] != -1) return dp[n]; // memoization
+using namespace std;
 
-  int result = f(n-1) + 1;
-  if(n%3 == 0) result = std::min(result, f(n/3) + 1);
-  if(n%2 == 0) result = std::min(result, f(n/2) + 1);
-  dp[n] = result;
-  return result;
-}
+int dp[MAX];
 int main(){
-  int N;
-  scanf("%d",&N);
-  std::fill(dp, dp+MAX, -1);
-  printf("%d",f(N));
+  int n;
+  scanf("%d",&n);
+  dp[1] = 0;
+  dp[2] = 1;
+  dp[3] = 1;
+  for(int i=4;i<=n;i++){
+    int tmp = dp[i-1];
+    if(i%3 == 0){
+      tmp = min(dp[i/3], tmp);
+    }
+    else if(i%2 == 0){
+      tmp = min(dp[i/2], tmp);
+    }
+    dp[i] = tmp + 1;
+  }
+  printf("%d",dp[n]);
   
   return 0;
 }
