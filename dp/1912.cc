a671ed5bd48404aa5dc9fc0b51fc234a67280473
nopanderer <hyunjin7857@gmail.com>
2018-12-30 19:53:37 +0900

    solving

---
+++ dp/1912.cc
@@ -0,0 +1,19 @@
+/**
+ * 연속합
+ * 1912
+ */
+#include <cstdio>
+
+int dp[100001];
+int a[100001];
+int main(){
+  int n;
+  scanf("%d",&n);
+  for(int i=0;i<n;i++)
+    scanf("%d",&a[i]);
+  dp[0] = a[0];
+  for(int i=1;i<n;i++)
+    dp[i] = 
+
+  return 0;
+}
