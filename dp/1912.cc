f5e6f2ad3e018f09b8cd74ec964e86ed836498f5
nopanderer <hyunjin7857@gmail.com>
2018-12-31 14:37:06 +0900

    연속합

--- dp/1912.cc
+++ dp/1912.cc
@@ -3,17 +3,26 @@
  * 1912
  */
 #include <cstdio>
+#define max(a,b) (((a)>(b))?(a):(b))
 
-int dp[100001];
+int d[100001];
 int a[100001];
 int main(){
   int n;
   scanf("%d",&n);
   for(int i=0;i<n;i++)
     scanf("%d",&a[i]);
-  dp[0] = a[0];
-  for(int i=1;i<n;i++)
-    dp[i] = 
+
+  d[0] = a[0];
+  for(int i=1;i<n;i++){
+    d[i] = max(d[i-1]+a[i],a[i]);
+  }
+  int ans = d[0];
+  for(int i=1;i<n;i++){
+    ans = max(ans, d[i]);
+  }
+  printf("%d",ans);
+
 
   return 0;
 }
