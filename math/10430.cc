0465a6abefc808c374bf05cf028790799fbb35dd
nopanderer <hyunjin7857@gmail.com>
2019-01-02 22:42:02 +0900

    나머지

---
+++ math/10430.cc
@@ -0,0 +1,19 @@
+/**
+ * 나머지
+ * 10430
+ */
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  int A,B,C;
+  cin>>A>>B>>C;
+
+  cout<<(A+B)%C<<endl;
+  cout<<(A+B)%C<<endl;
+  cout<<(A*B)%C<<endl;
+  cout<<(A*B)%C<<endl;
+
+  return 0;
+}
