b4acc36052c8cd28144cf546787c6efb8af4c468
nopanderer <hyunjin7857@gmail.com>
2019-01-04 13:45:26 +0900

    수 정렬하기 2

---
+++ sort/2751.cc
@@ -0,0 +1,20 @@
+/**
+ * 수 정렬하기 2
+ * 2751
+ */
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main(){
+  int N;
+  int a[1000001];
+  cin>>N;
+  for(int i=0;i<N;i++)
+    cin>>a[i];
+  sort(a,a+N);
+  for(int i=0;i<N;i++)
+    cout<<a[i]<<'\n';
+
+  return 0;
+}
