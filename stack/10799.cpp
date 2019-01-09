321b0a26055827a79f57d4af3b5fea357f9f1147
nopanderer <hyunjin7857@gmail.com>
2019-01-09 15:13:08 +0900

    쇠막대기

--- stack/10799.cpp
+++ stack/10799.cpp
@@ -4,31 +4,27 @@
  */
 #include <cstdio>
 #include <stack>
-
 using namespace std;
 
-char s[100001];
 int main(){
+  char s[100001];
   stack<int> st;
   scanf("%s",s);
-  int sum=0;
-
+  int ans=0;
   for(int i=0;s[i];i++){
-    if(s[i] == '(')
-      st.push(i);
-    else{
-      if(i-st.top() == 1){
-        st.pop();
-        sum += st.size();
+    if(s[i] == '('){
+      st.push(s[i]);
+    }
+    else if(s[i] == ')'){
+      st.pop();
+      if(s[i-1] == '('){
+        ans += st.size();
       }
       else{
-        st.pop();
-        sum += 1;
+        ans += 1;
       }
-    } 
+    }
   }
-
-  printf("%d",sum);
-
+  printf("%d\n",ans);
   return 0;
 }
